f5c5af98ccaf54c3f410063dc38d7c2b262c4925
Fun Fund <yo.myevm@gmail.com>
2025-01-14 01:22:28 +0000

    .

--- src/components/CreateComplex.tsx
+++ src/components/CreateComplex.tsx
@@ -4,12 +4,8 @@ import ProtocolHorizontalBar from "../components/ProtocolHorizontalBar"; // Impo
 const CreateComplex: React.FC = () => {
   return (
     <div className="bg-gray-900 p-8 rounded shadow-lg border-2 border-accent1">
-      <h2 className="text-3xl font-bold text-[#fd01f5]">Create Complex</h2>
-
-      {/* Add the ProtocolHorizontalBar below the description */}
-      <div className="mt-8">
-        <ProtocolHorizontalBar />
-      </div>
+      {/* Add the ProtocolHorizontalBar directly below the description */}
+      <ProtocolHorizontalBar />
     </div>
   );
 };
--- src/utils/protocols.ts
+++ src/utils/protocols.ts
@@ -11,97 +11,97 @@ export interface Protocol {
     {
       id: 1,
       name: "Utilites",
-      image: "https://example.com/ethereum-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 2,
       name: "Curve",
-      image: "https://example.com/bitcoin-logo.png", // Replace with actual image URL
+      image: "/curvelogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 3,
       name: "Aave",
-      image: "https://example.com/polygon-logo.png", // Replace with actual image URL
+      image: "/aavelogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 4,
       name: "Sushi",
-      image: "https://example.com/solana-logo.png", // Replace with actual image URL
+      image: "/sushilogo.jpg", // Replace with actual image URL
       description: ".",
     },
     {
       id: 5,
       name: "Uniswap",
-      image: "https://example.com/bsc-logo.png", // Replace with actual image URL
+      image: "/uniswaplogo.jpg", // Replace with actual image URL
       description: ".",
     },
     {
       id: 6,
       name: "Opensea",
-      image: "https://example.com/avalanche-logo.png", // Replace with actual image URL
+      image: "/opensealogo.webp", // Replace with actual image URL
       description: ".",
     },
     {
       id: 7,
       name: "Stargate",
-      image: "https://example.com/cardano-logo.png", // Replace with actual image URL
+      image: "/stargatelogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 8,
       name: "Protocol8",
-      image: "https://example.com/polkadot-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 9,
       name: "Protocol9",
-      image: "https://example.com/polkadot-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 10,
       name: "Protocol10",
-      image: "https://example.com/ethereum-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 11,
       name: "Protocol11",
-      image: "https://example.com/bitcoin-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 12,
       name: "Protocol12",
-      image: "https://example.com/polygon-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 13,
       name: "Protocol13",
-      image: "https://example.com/solana-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 14,
       name: "Protocol14",
-      image: "https://example.com/bsc-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 15,
       name: "Protocol15",
-      image: "https://example.com/avalanche-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
     {
       id: 16,
       name: "Protocol16",
-      image: "https://example.com/cardano-logo.png", // Replace with actual image URL
+      image: "/isailogo.png", // Replace with actual image URL
       description: ".",
     },
 
