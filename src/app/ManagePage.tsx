5f37889f77c55c1199a876258058d760cf54aa4f
Fun Fund <yo.myevm@gmail.com>
2025-01-15 20:37:04 +0000

    .

--- src/app/ManagePage.tsx
+++ src/app/ManagePage.tsx
@@ -13,33 +13,35 @@ const ManagePage: React.FC = () => {
   return (
     <section className="text-center max-w-screen-lg mx-auto py-8">
       <h1 className="text-5xl font-bold text-[#fd01f5] mt-10">
-        Manage your Smart NFT Brain
+        Manage your Agent Core Brain
       </h1>
       <p className="text-3xl text-accent1 mt-4">
         Turn any NFT into a Smart NFT by Equipping an ISAI Brain
       </p>
 
-      {/* Toggleable cards */}
+      {/* Toggleable buttons */}
       <div className="flex justify-center space-x-8 mt-8">
-        <div
+        <button
           onClick={() => handleCardSelection("ISAI AGENT")}
-          className={`cursor-pointer p-6 bg-gray-800 rounded-lg shadow-lg w-1/3 ${
-            selectedOption === "ISAI AGENT" ? "bg-[#fd01f5] text-white" : "text-gray-300"
+          className={`transition-transform transform hover:scale-105 focus:scale-105 focus:outline-none px-8 py-4 rounded-full shadow-lg font-bold text-lg ${
+            selectedOption === "ISAI AGENT"
+              ? "bg-[#fd01f5] text-white"
+              : "bg-gray-800 text-gray-300 hover:bg-gray-700"
           }`}
         >
-          <h2 className="text-2xl font-bold mb-4">ISAI AGENT</h2>
-          <p>Select this option to equip an ISAI Agent to your NFT.</p>
-        </div>
+          ISAI Agent Cores
+        </button>
 
-        <div
+        <button
           onClick={() => handleCardSelection("YourNFT")}
-          className={`cursor-pointer p-6 bg-gray-800 rounded-lg shadow-lg w-1/3 ${
-            selectedOption === "YourNFT" ? "bg-[#fd01f5] text-white" : "text-gray-300"
+          className={`transition-transform transform hover:scale-105 focus:scale-105 focus:outline-none px-8 py-4 rounded-full shadow-lg font-bold text-lg ${
+            selectedOption === "YourNFT"
+              ? "bg-[#fd01f5] text-white"
+              : "bg-gray-800 text-gray-300 hover:bg-gray-700"
           }`}
         >
-          <h2 className="text-2xl font-bold mb-4">YourNFTs</h2>
-          <p>Select this option to equip your own NFT with an ISAI Brain.</p>
-        </div>
+          Your Wallet NFTs
+        </button>
       </div>
 
       {/* Form or additional options based on selected option */}
@@ -48,9 +50,7 @@ const ManagePage: React.FC = () => {
           <h3 className="text-xl text-white mb-6">
             You selected: {selectedOption}
           </h3>
-          <p className="text-gray-400 text-center">
-            Choose an Owned NFT
-          </p>
+          <p className="text-gray-400 text-center">Choose an Owned NFT</p>
         </div>
       )}
     </section>
