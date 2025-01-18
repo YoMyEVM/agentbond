47399ab2396c5f16a1e852a739e5e99688508f63
Fun Fund <yo.myevm@gmail.com>
2025-01-18 16:16:18 +0000

    .

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -5,8 +5,12 @@ export interface Chain {
   symbol?: string;
   explorerUrl?: string;
   image?: string;
-  color: string;  // Added color property
+  color: string;  
+  GPOaddress: string;
+  WETHaddress: string;
+  GPOexplorerurl: string;
 }
+
 
 export const colors: { [key in 'polygon' | 'baseChain' | 'apeChain' | 'abstract' | 'optimism' | 'beraChain' | 'arbitrum' | 'uniChain']: string } = {
   polygon: '#8247E5',  
@@ -18,7 +22,6 @@ export const colors: { [key in 'polygon' | 'baseChain' | 'apeChain' | 'abstract'
   arbitrum: '#2bbdfa',   
   uniChain: '#FF007A',   
 };
-
 export const chains: Chain[] = [
   {
     name: "Base",
@@ -27,7 +30,10 @@ export const chains: Chain[] = [
     symbol: "BASE",
     explorerUrl: "https://basescan.org/",
     image: "/basechainlogo.svg",
-    color: colors.baseChain,  // Assigning color
+    color: colors.baseChain,
+    GPOaddress: "0xF30667dBd90868dB7AAb74Ad87E9671B8eED7F99",
+    WETHaddress: "0x4200000000000000000000000000000000000006",
+    GPOexplorerurl: "https://basescan.org/address/0xF30667dBd90868dB7AAb74Ad87E9671B8eED7F99"
   },
   {
     name: "Optimism",
@@ -36,7 +42,10 @@ export const chains: Chain[] = [
     symbol: "OP",
     explorerUrl: "https://optimistic.etherscan.io",
     image: "/optimismlogo.svg",
-    color: colors.optimism,  // Assigning color
+    color: colors.optimism,
+    GPOaddress: "0xF3F029Cdd7586Fc5F705bd206339507F1fbEd275",
+    WETHaddress: "0x4200000000000000000000000000000000000006",
+    GPOexplorerurl: "https://optimistic.etherscan.io/address/0xF3F029Cdd7586Fc5F705bd206339507F1fbEd275"
   },
   {
     name: "Polygon",
@@ -45,7 +54,10 @@ export const chains: Chain[] = [
     symbol: "MATIC",
     explorerUrl: "https://polygonscan.com",
     image: "/polygonlogo.svg",
-    color: colors.polygon,  // Assigning color
+    color: colors.polygon,
+    GPOaddress: "0x1FD729f324B6FDd4F12464f970b587139B24b005",
+    WETHaddress: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
+    GPOexplorerurl: "https://polygonscan.com/address/0x1FD729f324B6FDd4F12464f970b587139B24b005"
   },
   {
     name: "Arbitrum",
@@ -54,7 +66,10 @@ export const chains: Chain[] = [
     symbol: "ARB",
     explorerUrl: "https://arbiscan.io",
     image: "/arbitrumlogo.svg",
-    color: colors.arbitrum,  // Assigning color
+    color: colors.arbitrum,
+    GPOaddress: "0xA49794ffA36f26C403000d1F2A501c5A78Db6b47",
+    WETHaddress: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    GPOexplorerurl: "https://arbiscan.io/address/0xA49794ffA36f26C403000d1F2A501c5A78Db6b47"
   },
   {
     name: "ApeChain",
@@ -63,33 +78,10 @@ export const chains: Chain[] = [
     symbol: "APE",
     explorerUrl: "https://apescan.io/",
     image: "/apechainlogo.png",
-    color: colors.apeChain,  // Assigning color
+    color: colors.apeChain,
+    GPOaddress: "0x1b2aE5F73Fd1db012B982DF554509eF8b3efb6c4",
+    WETHaddress: "0xcF800F4948D16F23333508191B1B1591daF70438",
+    GPOexplorerurl: "https://apechain.calderaexplorer.xyz/address/0x1b2aE5F73Fd1db012B982DF554509eF8b3efb6c4"
   },
-  {
-    name: "Abstract",
-    id: 2741,
-    rpc: "https://abstract-testnet.g.alchemy.com/v2/Q-PUqxJnC1RsVq_KZG1KeNSxJOHZXs4g",
-    symbol: "ABS",
-    explorerUrl: "https://explorer.testnet.abs.xyz/",
-    image: "/abstractlogo.jpg",
-    color: colors.abstract,  // Assigning color
-  },
-  {
-    name: "Unichain",
-    id: 130,
-    rpc: "https://unichain-sepolia.g.alchemy.com/v2/Q-PUqxJnC1RsVq_KZG1KeNSxJOHZXs4g",
-    symbol: "UNI",
-    explorerUrl: "https://sepolia.uniscan.xyz/",
-    image: "/unichainlogo.svg",
-    color: colors.uniChain,  // Assigning color
-  },
-  {
-    name: "BeraChain",
-    id: 80084,
-    rpc: "https://berachain-bartio.g.alchemy.com/v2/Q-PUqxJnC1RsVq_KZG1KeNSxJOHZXs4g",
-    symbol: "BERA",
-    explorerUrl: "https://berachain.nodefleet.net/",
-    image: "/berachainlogo.webp",
-    color: colors.beraChain,  // Assigning color
-  },
+
 ];
