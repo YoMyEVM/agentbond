4b3ec44d329515a41eb2d613b374a78f646c4a04
Fun Fund <yo.myevm@gmail.com>
2025-01-18 07:48:49 +0000

    .

--- src/components/CreatePlugin.tsx
+++ src/components/CreatePlugin.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const CreatePlugin: React.FC = () => {
   return (
     <div className="bg-gray-900 p-8 rounded shadow-lg border-2 border-accent1">
-      <h2 className="text-3xl font-bold text-[#fd01f5]">Create Web2 Plugin</h2>
+      <h2 className="text-3xl font-bold text-[#fd01f5]">Create Web2 Plugin Coming Soon</h2>
     </div>
   );
 };
