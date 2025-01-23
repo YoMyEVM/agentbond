0345e5047545591d49437a9d07caf93e2c2a82c6
Fun Fund <yo.myevm@gmail.com>
2025-01-23 02:43:31 +0000

    .

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -3,6 +3,7 @@ export interface Token {
   name: string;
   address: string;
   symbol: string;
+  decimals: string;
   image: string;
   color: string;
   progress?: number[];
@@ -16,6 +17,7 @@ export const baseTokens: Token[] = [
     name: "USDbC",
     address: "0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA",
     symbol: "USDbC",
+    decimals: "6",
     image: "/usdbclogo.png",
     color: "#fff", // Coinbase blue for USDbC
     dexpool: "0x0E635F8EeED4F7279d56692D552F034ECE136019",
@@ -27,6 +29,7 @@ export const baseTokens: Token[] = [
     name: "WETH",
     address: "0x4200000000000000000000000000000000000006",
     symbol: "WETH",
+    decimals: "18",
     image: "/wethlogo.png",
     color: "#ffffff", // White for WETH
     dexpool:"0xd0b53D9277642d899DF5C87A3966A349A798F224",
@@ -37,6 +40,7 @@ export const baseTokens: Token[] = [
     name: "GenCredits",
     address: "Not live yet",
     symbol: "GenCredits",
+    decimals: "18",
     image: "/studiologo.png",
     color: "#01ecec", 
     dexpool: "Placeholder",
@@ -47,6 +51,7 @@ export const baseTokens: Token[] = [
     name: "cbBTC",
     address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
     symbol: "cbBTC",
+    decimals: "18",
     image: "/cbbtclogo.png",
     color: "#0052FF", // Coinbase blue for cbBTC
     dexpool: "0xfBB6Eed8e7aa03B138556eeDaF5D271A5E1e43ef",
@@ -57,6 +62,7 @@ export const baseTokens: Token[] = [
     name: "cbETH",
     address: "0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22",
     symbol: "cbETH",
+    decimals: "18",
     image: "/cbethlogo.png",
     color: "#6697ff", // Coinbase blue for cbETH
     dexpool: "0x9BB646BF0F4Da44bfaF3d899e774DE065731EDFe",
@@ -72,6 +78,7 @@ export const optimismTokens: Token[] = [
     name: "USDC",
     address: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
     symbol: "USDC",
+    decimals: "6",
     image: "/usdclogo.png",
     color: "#2775ca", // Coinbase blue for USDC
     dexpool: "0x1fb3cf6e48f1e7b10213e7b6d87d4c073c7fdb7b",
@@ -82,6 +89,7 @@ export const optimismTokens: Token[] = [
     name: "WETH",
     address: "0x4200000000000000000000000000000000000006",
     symbol: "WETH",
+    decimals: "18",
     image: "/wethlogo.png",
     color: "#ffffff", // White for WETH
     dexpool: "0x03af20bdaaffb4cc0a521796a223f7d85e2aac31",
@@ -92,6 +100,7 @@ export const optimismTokens: Token[] = [
     name: "GenCredits",
     address: "Not live yet",
     symbol: "GenCredits",
+    decimals: "18",
     image: "/studiologo.png",
     color: "#01ecec", 
     dexpool: "Placeholder",
@@ -102,6 +111,7 @@ export const optimismTokens: Token[] = [
     name: "OP",
     address: "0x4200000000000000000000000000000000000042",
     symbol: "OP",
+    decimals: "18",
     image: "/optimismlogo.svg",
     color: "#ed1e2c", // Coinbase blue for OP
     dexpool:"0xfc1f3296458f9b2a27a0b91dd7681c4020e09d05",
@@ -112,6 +122,7 @@ export const optimismTokens: Token[] = [
     name: "SNX",
     address: "0x8700dAec35aF8Ff88c16BdF0418774CB3D7599B4",
     symbol: "SNX",
+    decimals: "18",
     image: "/snxlogo.png",
     color: "#01cefd", // Coinbase blue for SNX
     dexpool: "0x0392b358ce4547601befa962680bede836606ae2",
@@ -126,6 +137,7 @@ export const polygonTokens: Token[] = [
     name: "USDC",
     address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
     symbol: "USDC",
+    decimals: "6",
     image: "/usdclogo.png",
     color: "#2775ca", // Coinbase blue for USDC
     dexpool: "0xb6e57ed85c4c9dbfef2a68711e9d6f36c56e0fcb",
@@ -136,6 +148,7 @@ export const polygonTokens: Token[] = [
     name: "WETH",
     address: "0x11CD37bb86F65419713f30673A480EA33c826872",
     symbol: "WETH",
+    decimals: "18",
     image: "/wethlogo.png",
     color: "#ffffff", // White for WETH
     dexpool: "0x4ccd010148379ea531d6c587cfdd60180196f9b1",
@@ -146,6 +159,7 @@ export const polygonTokens: Token[] = [
     name: "GenCredits",
     address: "Not live yet",
     symbol: "GenCredits",
+    decimals: "18",
     image: "/studiologo.png",
     color: "#01ecec", 
     dexpool: "Placeholder",
@@ -156,6 +170,7 @@ export const polygonTokens: Token[] = [
     name: "WPOL",
     address: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
     symbol: "POL",
+    decimals: "18",
     image: "/pollogo.png",
     color: "#8347e5", // Coinbase blue for POL
     dexpool: "0xa374094527e1673a86de625aa59517c5de346d32",
@@ -166,6 +181,7 @@ export const polygonTokens: Token[] = [
     name: "OLAS",
     address: "0xFEF5d947472e72Efbb2E388c730B7428406F2F95",
     symbol: "OLAS",
+    decimals: "18",
     image: "/olaslogo.png",
     color: "#ffffff", // OLAS color
     dexpool: "0x2b274ed54b94b44909085d0cac36171f03109bb9",
@@ -179,6 +195,7 @@ export const arbitrumTokens: Token[] = [
     name: "USDC",
     address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
     symbol: "USDC",
+    decimals: "6",
     image: "/usdclogo.png",
     color: "#2775ca", // Coinbase blue for USDC
     dexpool: "0xc6962004f452be9203591991d15f6b388e09e8d0",
@@ -190,6 +207,7 @@ export const arbitrumTokens: Token[] = [
     name: "WETH",
     address: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
     symbol: "WETH",
+    decimals: "18",
     image: "/wethlogo.png",
     color: "#ffffff", // White for WETH
     dexpool: "0x641c00a822e8b671738d32a431a4fb6074e5c79d",
@@ -200,6 +218,7 @@ export const arbitrumTokens: Token[] = [
     name: "GenCredits",
     address: "Not live yet",
     symbol: "GenCredits",
+    decimals: "18",
     image: "/studiologo.png",
     color: "#01ecec", 
     dexpool: "Placeholder",
@@ -210,6 +229,7 @@ export const arbitrumTokens: Token[] = [
     name: "ARB",
     address: "Native",
     symbol: "ARB",
+    decimals: "18",
     image: "/arblogo.png",
     color: "#12AAFF", // Coinbase blue for ARB
     dexpool: "0xc6f780497a95e246eb9449f5e4770916dcd6396a",
@@ -221,6 +241,7 @@ export const arbitrumTokens: Token[] = [
     name: "SOL",
     address: "0x2bcC6D6CdBbDC0a4071e48bb3B969b06B3330c07",
     symbol: "SOL",
+    decimals: "18",
     image: "/sollogo.png",
     color: "#00FFA3", // Coinbase blue for ARB
     dexpool: "0x622B5186384783BB805c12A808cCF07F41DE1Ff0",
@@ -237,6 +258,7 @@ export const apeChainTokens: Token[] = [
     name: "ApeUSD",
     address: "0xA2235d059F80e176D931Ef76b6C51953Eb3fBEf4",
     symbol: "ApeUSD",
+    decimals: "18",
     image: "/usdclogo.png",
     color: "#2775ca", // Coinbase blue for ApeUSD
     dexpool: "0x99556e210123da382eded3c72aa8dcb605c3c435",
@@ -248,6 +270,7 @@ export const apeChainTokens: Token[] = [
     name: "ApeETH",
     address: "0xcF800F4948D16F23333508191B1B1591daF70438",
     symbol: "ApeETH",
+    decimals: "18",
     image: "/wethlogo.png",
     color: "#ffffff", // White for ApeETH
     dexpool: "0xea03aedda280ed21f39cb6084b4f84bacd3ebc31",
@@ -258,6 +281,7 @@ export const apeChainTokens: Token[] = [
     name: "GenCredits",
     address: "Not live yet",
     symbol: "GenCredits",
+    decimals: "18",
     image: "/studiologo.png",
     color: "#01ecec", 
     dexpool: "Placeholder",
@@ -268,6 +292,7 @@ export const apeChainTokens: Token[] = [
     name: "WAPE",
     address: "0x48b62137EdfA95a428D35C09E44256a739F6B557",
     symbol: "WAPE",
+    decimals: "18",
     image: "/apelogo.png",
     color: "#0148d7", // Coinbase blue for APE
     dexpool: "0xcbbe0a6d394b34a486fe9c50bf37bf835cbbae51",
@@ -279,6 +304,7 @@ export const apeChainTokens: Token[] = [
     name: "SUPER",
     address: "0xe31C676d8235437597581b44c1c4f8A30e90b38a",
     symbol: "SUPER",
+    decimals: "18",
     image: "/superlogo.png",
     color: "#71f294", // Coinbase blue for SUPER
     dexpool: "0x8b2bc7d000d395cba8442e4d835a5e77e4f9d1fe",
@@ -292,6 +318,7 @@ export const abstractTokens: Token[] = [
     name: "USDC",
     address: "Not live yet",
     symbol: "USDC",
+    decimals: "6",
     image: "/usdclogo.png",
     color: "#2775ca", // Coinbase blue for USDC
     dexpool: "Placeholder",
@@ -304,6 +331,7 @@ export const abstractTokens: Token[] = [
     name: "WETH",
     address: "Not live yet",
     symbol: "WETH",
+    decimals: "18",
     image: "/wethlogo.png",
     color: "#ffffff", // White for WETH
     dexpool: "Placeholder",
@@ -315,6 +343,7 @@ export const abstractTokens: Token[] = [
     name: "GenCredits",
     address: "Not live yet",
     symbol: "GenCredits",
+    decimals: "18",
     image: "/studiologo.png",
     color: "#01ecec", 
     dexpool: "Placeholder",
@@ -325,6 +354,7 @@ export const abstractTokens: Token[] = [
     name: "PENGU",
     address: "Not live yet",
     symbol: "PENGU",
+    decimals: "18",
     image: "/pengulogo.png",
     color: "#83a9f6", // PENGU color
     dexpool: "Placeholder",
@@ -339,6 +369,7 @@ export const unichainTokens: Token[] = [
     name: "USDC",
     address: "Not live yet",
     symbol: "USDC",
+    decimals: "6",
     image: "/usdclogo.png",
     color:" #2775ca", // Coinbase blue for USDC
     dexpool: "Placeholder",
@@ -351,6 +382,7 @@ export const unichainTokens: Token[] = [
     name: "WETH",
     address: "Not live yet",
     symbol: "WETH",
+    decimals: "18",
     image: "/wethlogo.png",
     color: "#ffffff", // White for WETH
     dexpool: "Placeholder",
@@ -361,6 +393,7 @@ export const unichainTokens: Token[] = [
     name: "GenCredits",
     address: "Not live yet",
     symbol: "GenCredits",
+    decimals: "18",
     image: "/studiologo.png",
     color: "#01ecec", 
     dexpool: "Placeholder",
@@ -371,6 +404,7 @@ export const unichainTokens: Token[] = [
     name: "UNI",
     address: "Not live yet",
     symbol: "UNI",
+    decimals: "18",
     image: "/unilogos.png",
     color: "#FF007A", // Coinbase blue for UNI
     dexpool: "Placeholder",
@@ -384,6 +418,7 @@ export const beraChainTokens: Token[] = [
     name: "HONEY",
     address: "Not live yet",
     symbol: "HONEY",
+    decimals: "6",
     image: "/honeylogo.png",
     color: "#e29d42", // HONEY color
     dexpool: "Placeholder",
@@ -394,6 +429,7 @@ export const beraChainTokens: Token[] = [
     name: "WETH",
     address: "Not live yet",
     symbol: "WETH",
+    decimals: "18",
     image: "/wethlogo.png",
     color: "#ffffff", // White for WETH 
     dexpool: "Placeholder",
@@ -405,6 +441,7 @@ export const beraChainTokens: Token[] = [
     name: "GenCredits",
     address: "Not live yet",
     symbol: "GenCredits",
+    decimals: "18",
     image: "/studiologo.png",
     color: "#01ecec", 
     dexpool: "Placeholder",
@@ -415,6 +452,7 @@ export const beraChainTokens: Token[] = [
     name: "BERA",
     address: "Not live yet",
     symbol: "BERA",
+    decimals: "18",
     image: "/beralogo.png",
     color: "#78350f", // HONEY color
     dexpool: "Placeholder",
